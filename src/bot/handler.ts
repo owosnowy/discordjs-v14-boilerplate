e08c07ccd9520d4d2b813d5a5627df9e17627510
sownyluv <snowyfe@proton.me>
2024-10-03 17:44:25 -0500

    command handling

--- src/bot/handler.ts
+++
@@ -1,9 +0,0 @@
-import fg from "fast-glob";
-import path from "path";
-
-export async function loadCommands() {
-    const filePaths: string[] = await fg(
-        `${__dirname.replace(/\\/g, '/')}/../commands/**/*{.ts,.js}`
-    );
-    return loadCommands;
-}
